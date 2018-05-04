c071e38f9bea3bcd9d19b72885c3d2a06c06f045
srappaport <sarah.rappaport@greendot.org>
2018-05-04 16:38:28 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,13 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+row1 = "   |   |   "
+row2=  "-----------"
+
+puts row1
+puts row2
+puts row1
+puts row2
+puts row1
+end
+
+display_board
